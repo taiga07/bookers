e6155fc95dc211f09cac5960ce635fc8df38d3fc
EC2 Default User <ec2-user@ip-172-31-66-105.ec2.internal>
2021-11-08 18:07:36 +0000

    [add]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,43 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books=Book.all
+  end
+  
+  def show
+    @book=Book.find(params[:id])
   end
 
   def new
+    @book=Book.new
+  end
+  
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to :action => 'index'
+    # ネット参照　※book_pathではエラー
+  end
+
+  private
+  def book_params
+  params.permit( :title, :body)
+  end
+
 end
